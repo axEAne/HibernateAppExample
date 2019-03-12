72c452efa7291341ec21e4b027a695dbee0bd958
Alex Kononchuk <axeane@gmail.com>
2019-03-12 19:39:34 +0100

    application with some bugs

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import models.Auto;
+import models.User;
+
+import java.util.List;
+
+public interface UserDao {
+    public User findById(int id);
+    public void save(User user);
+    public void update(User user);
+    public void delete(User user);
+    public Auto findAutoById(int id);
+    public List<User> findAll();
+}
---
+++ src/main/java/dao/UserDaoImpl.java
@@ -0,0 +1,55 @@
+package dao;
+
+import models.Auto;
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateSessionFactoryUtil;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao{
+    @Override
+    public User findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
+    @Override
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+    }
+
+    @Override
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(user);
+        tx1.commit();
+        session.close();
+    }
+
+    @Override
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+    }
+
+    @Override
+    public Auto findAutoById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        //It doesen't work, so I'll fix that later.
+        //List<User> users = (List<User>)  HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("from User", User.class).list();
+        return null;
+    }
+}
